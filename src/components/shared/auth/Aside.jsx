a9938464f3e67cffa42aafbf0c7ea43ffbda7b45
Kejdi Nela <kejdiinela@gmail.com>
2025-04-28 18:36:41 +0200

    created login page

---
+++ src/components/shared/auth/Aside.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const Aside = () => {
+  return (
+    <div className="relative overflow-hidden bg-green-900 md:flex hidden w-1/2 flex-col p-0">
+  
+    <div className="absolute inset-0 flex items-center justify-center">
+      <h1 className="text-transparent bg-clip-text  bg-gradient-to-r from-yellow-700 to-yellow-300  md:text-8xl text-center">
+        Moxxa <span className="">caffe</span>
+      </h1>
+    </div>
+  </div>
+    
+  )
+}
+
+export default Aside 
