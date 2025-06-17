b398f9d9fd55093355658eff6724027ee57eee2c
leonard dani <leonarddani02@gmail.com>
2025-06-17 16:33:07 +0200

    db-connected

--- client/src/components/shared/auth/SignupForm.jsx
+++ client/src/components/shared/auth/SignupForm.jsx
@@ -19,9 +19,6 @@ import { Input } from "@/components/ui/input";
 const formSchema = z
   .object({
     name: z.string().min(2, { message: "Name must be at least 2 characters" }),
-    username: z
-      .string()
-      .min(2, { message: "Username must be at least 2 characters" }),
     email: z.string().email({ message: "Please enter a valid email" }),
     password: z
       .string()
@@ -42,7 +39,6 @@ const SignupForm = () => {
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
-      username: "",
       email: "",
       password: "",
       confirmPassword: "",
